b4199a9853421fe6ed523eda5e9307136d183c0e
Natanael Bezerra <nsbbezerra@hotmail.com>
2021-12-03 15:45:52 -0300

    change trophys table

--- src/migrations/20211122201427_create_table_of_trophys.js
+++ src/migrations/20211122201427_create_table_of_trophys.js
@@ -1,27 +1,26 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('trophys', function(table) {
-    table.increments('id');
+exports.up = function (knex) {
+  return knex.schema.createTable("trophys", function (table) {
+    table.increments("id");
     table
-      .integer('raffle_id')
-      .references('raffles.id')
+      .integer("raffle_id")
+      .references("raffles.id")
       .notNullable()
-      .onDelete('CASCADE');
-    table.string('title');
-    table.string('description');
-    table.string('number');
-    table.string('name_client');
-    table.string('id_client');
-    table.json('address');
+      .onDelete("CASCADE");
+    table.string("title");
+    table.string("description");
+    table.string("number");
+    table.json("name_client");
+    table.string("client_identify");
+    table.json("address");
     table
-      .enu('status', ['waiting', 'drawn'])
+      .enu("status", ["waiting", "drawn"])
       .notNullable()
-      .defaultTo('waiting');
-
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
+      .defaultTo("waiting");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
   });
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTable('trophys');
+exports.down = function (knex) {
+  return knex.schema.dropTable("trophys");
 };
