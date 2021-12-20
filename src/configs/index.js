1167a21c8bdaf1f0ffe4eacb2b1b13e756d0f79f
Natanael Bezerra <nsbbezerra@hotmail.com>
2021-12-20 09:43:30 -0300

    add token

--- src/configs/index.js
+++ src/configs/index.js
@@ -5,7 +5,8 @@ module.exports = {
   url: tokens.AMBIENT === "dev" ? tokens.API_DEV : tokens.API,
   site_url: tokens.AMBIENT === "dev" ? tokens.SITE_DEV : tokens.SITE,
   payment_url: tokens.MP,
-  payment_token: tokens.MP_AMBIENT === "dev" ? tokens.TEST : tokens.PRODUCTION,
+  payment_token:
+    "3430681041131246-112915-005df21db1e4061828391a4de2393ce5-681594284",
   pixTax: 0.99,
   cardTax: 4.99,
   boleto: 3.49,
